63e7d72eaaac2224bb1fbf19ebbdd310baed55be
Mojtaba Parsaee <48765348+parsaeemojtaba@users.noreply.github.com>
2022-04-15 14:51:29 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -3,11 +3,11 @@
 import numpy as np
 import RaspiCamera as RaspiCamera
 
-CaptureMainDirName='Camera_Captures'
-RaspiCam = RaspiCamera.RaspiCamera(CaptureMainDirName)
-
+RaspiCam = RaspiCamera.RaspiCamera()
 ShutterSpeed=6400
 runStereo=RaspiCam.CaptureSinglePicture(3125, 2500, 'ImgSingle', '.jpg', 100, ShutterSpeed, 'off', 2.88671875, 1.8359375)
 
-ShutterSpeedArray=np.array([3200, 4800, 6400, 10000, 20000])
+CaptureMainDirName='Camera_Captures'
+RaspiCam = RaspiCamera.RaspiCamera(MakeTimelapsCaptrueDir=True, CaptureMainDirName=CaptureMainDirName)
+ShutterSpeedArray=np.array([4800, 6400, 10000])
 runStereo=RaspiCam.CaptureMultiplePicture(3125, 2500, 'ImgBracketed', '.jpg', 100, ShutterSpeedArray, 'off', 2.88671875, 1.8359375)
